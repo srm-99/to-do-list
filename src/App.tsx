ddb410dcb5a1e5a31edd063a7bbe98ca4de0e126
srm-99 <santiago.ramirezm99@gmail.com>
2021-09-04 09:43:02 -0500

    create project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ToDoList from "./components/ToDoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="container">
+            <header>
+                <h1>To do List!</h1>
+            </header>
+            <main>
+                <div className="input-section">
+                    <input type="text" placeholder="Type your taks here!" />
+                    <button>➕</button>
+                </div>
+
+                <div className="list-section">
+                    <ul>
+                        <li>
+                            <p>wake up</p>
+                        </li>
+                        <li>
+                            <p>have breakfast</p>
+                        </li>
+                        <li>
+                            <p>brush my teeth</p>
+                        </li>
+                        <li>
+                            <p>take a shower</p>
+                        </li>
+                    </ul>
+                </div>
+            </main>
+        </div>
+    );
 }
-
-export default App;
