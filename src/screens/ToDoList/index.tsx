daaece06222baedd19c2c5e0f5d7ac64f30d49b1
srm-99 <santiago.ramirezm99@gmail.com>
2021-09-07 13:40:35 -0500

    adjust css minimal design and add delete all button

--- src/screens/ToDoList/index.tsx
+++ src/screens/ToDoList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, KeyboardEvent, MouseEvent } from "react";
+import React, { useState, ChangeEvent, MouseEvent } from "react";
 
 // Utils
 import { parseDate, handleEnter } from "./utils";
@@ -6,9 +6,17 @@ import { parseDate, handleEnter } from "./utils";
 // Types
 import { ToDoListStateType } from "./types";
 
+//Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faPlus } from "@fortawesome/free-solid-svg-icons";
+
 const ToDoList = () => {
+    // ----------------------------------------- Use States
+
     const [tasks, setTasks] = useState<ToDoListStateType[]>([]);
     const [inputValue, setInputValue] = useState<string>("");
+
+    // ----------------------------------------- Functions
 
     const handleInputValue = (event: ChangeEvent<HTMLInputElement>) => {
         const task = event.target.value;
@@ -16,7 +24,6 @@ const ToDoList = () => {
     };
 
     const addTask = () => {
-        console.log({ inputValue });
         if (!inputValue) {
             alert("the input is empty");
             return;
@@ -31,49 +38,94 @@ const ToDoList = () => {
         setTasks(newTasks);
         setInputValue("");
     };
+
     const handleCompletedTask = (index: number) => {
-        const newTasks = [...tasks];
-        newTasks[index].completed = !newTasks[index].completed;
-        setTasks(newTasks);
         /*
         1. crear un nuevo array a partir del anterior
         2. acceder al objeto del nuevo Array usando el index
         3. cambiar el estado de la propiedad
         4. hacer el setTasks con el nuevo array 
         */
+        const newTasks = [...tasks];
+        newTasks[index].completed = !newTasks[index].completed;
+        setTasks(newTasks);
     };
 
-    const deleteTask = (indexDelete: number) => {
+    const deleteTask = (
+        event: MouseEvent<HTMLButtonElement>,
+        indexDelete: number
+    ) => {
         /*
         1.crear un nuevo array a partir del anterior
         2. almacenar un filter al array
         3. dentro del filter validar si el index es igual al param de la funcion retorna false y sino retorna true
         4. hacer el setTasks del nuevo array 
         */
-        const filterTasks = tasks.filter(
-            (_, indexFilter) => indexFilter !== indexDelete
+        event.stopPropagation();
+        setTasks(
+            tasks.filter((_, indexFilter) => {
+                // console.log({
+                //     indexFilter,
+                //     indexDelete,
+                //     BooleanValue: indexFilter !== indexDelete,
+                // });
+                return indexFilter !== indexDelete;
+            })
         );
-        setTasks(filterTasks);
     };
 
+    const handleDeleteTasksCompleted = () => {
+        /*
+        - Como voy a actualizar una variable de estado (es un array) debo tener
+        presente que se debe retorna un nuevo array.
+
+        1. Crear un nuevo array.
+        2. Recorrer el array filtrando los elementos que tienen {completed: true}
+        3. realizer el setTasks con el nuevo array
+        al usar Array.filter() el primer 1 & 2 se unen. al utilizarlo en el setTask que melo
+        */
+        setTasks(tasks.filter((task) => !task.completed));
+    };
+
+    // -----------------------------------------
     return (
-        <div className="container">
+        <>
             <header>
                 <h1>To do List!</h1>
             </header>
             <main>
-                <div className="input-section">
-                    <input
-                        type="text"
-                        placeholder="Type your taks here!"
-                        value={inputValue}
-                        onChange={handleInputValue}
-                        onKeyDown={handleEnter(() => addTask())}
-                    />
-                    <button onClick={addTask}>➕</button>
+                <div className="container">
+                    <div className="input-section">
+                        <input
+                            type="text"
+                            placeholder="Type your taks here!"
+                            value={inputValue}
+                            onChange={handleInputValue}
+                            onKeyDown={handleEnter(() => addTask())}
+                        />
+                        <button className="add-button" onClick={addTask}>
+                            <FontAwesomeIcon icon={faPlus} size="xs" />
+                        </button>
+                        <button
+                            className="delete-button"
+                            onClick={handleDeleteTasksCompleted}
+                        >
+                            <FontAwesomeIcon icon={faTrash} size="xs" />
+                        </button>
+                    </div>
                 </div>
-                <div className="list-section">
-                    <ul>
+                <hr
+                    style={{
+                        display: tasks.length ? "block" : "none",
+                    }}
+                />
+                <div
+                    className="container"
+                    style={{
+                        display: tasks.length ? "block" : "none",
+                    }}
+                >
+                    <ul className="to-do-list">
                         {tasks.map((task, index) => (
                             <li
                                 key={task.id}
@@ -83,34 +135,39 @@ const ToDoList = () => {
                                     handleCompletedTask(index)
                                 )}
                             >
-                                <div
+                                <p
                                     style={{
                                         textDecoration: task.completed
                                             ? "line-through"
                                             : "none",
+                                        color: task.completed
+                                            ? "var(--second-font-color)"
+                                            : "var(--font-color)",
                                     }}
                                 >
-                                    <p>{task.task}</p>
-                                    <div>
-                                        <strong className="li-strong">
-                                            {parseDate(task.dateCreated)}
-                                        </strong>
-                                        <button
-                                            onClick={() => deleteTask(index)}
-                                            onKeyDown={handleEnter(() =>
-                                                deleteTask(index)
-                                            )}
-                                        >
-                                            Delete
-                                        </button>
-                                    </div>
-                                </div>
+                                    {task.task}
+                                </p>
+                                <span>
+                                    Created on {parseDate(task.dateCreated)}
+                                </span>
+                                <button
+                                    onClick={(event) =>
+                                        deleteTask(event, index)
+                                    }
+                                >
+                                    <FontAwesomeIcon icon={faTrash} />
+                                </button>
                             </li>
                         ))}
                     </ul>
                 </div>
+                <hr
+                    style={{
+                        display: tasks.length ? "block" : "none",
+                    }}
+                />
             </main>
-        </div>
+        </>
     );
 };
 
--- src/screens/ToDoList/utils.ts
+++ src/screens/ToDoList/utils.ts
@@ -2,10 +2,16 @@ import { KeyboardEvent } from "react";
 
 export const parseDate = (dateNumber: number): string => {
     const date = new Date(dateNumber);
-    return `${date.getDay()}/${date.getMonth() + 1}/${date.getFullYear()}`
-}
+    const day = date.getDay() < 10 ? `0${date.getDay()}` : date.getDay();
+    const month =
+        date.getMonth() + 1 < 10
+            ? `0${date.getMonth() + 1}`
+            : date.getMonth() + 1;
+    return `${day}/${month}/${date.getFullYear()}`;
+};
 
 export const handleEnter = (fn: any) => (event: KeyboardEvent<any>) => {
-    event.stopPropagation()
-    event.code == "Enter" && fn()
-}
+    event.stopPropagation();
+    const allowedCodes = ["Enter", "NumpadEnter"];
+    allowedCodes.includes(event.code) && fn();
+};
